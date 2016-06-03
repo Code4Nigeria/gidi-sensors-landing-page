da30037ccc1b5ce34c290d4e936b4adf41efd3d4
TED <tedlade.tl@gmail.com>
2016-06-03 11:56:09 +0100

    Test

---
+++ javascript/app.js
@@ -0,0 +1,14 @@
+$(function() {
+
+	$('.contact').click(function(){
+	     $('.overlay').show();
+	});
+
+	$('#close').click(function(){
+	     $('.overlay').hide();
+	});
+
+	$('#send').click(function(){
+	     $('.confirmation').show();
+	});
+});
