0f9f49924a94b4c0d741088458da60e67121e3b7
TED <tedlade.tl@gmail.com>
2016-06-03 11:57:57 +0100

    Media files

---
+++ assets/js/app.js
@@ -0,0 +1,36 @@
+
+angular.module('app', [
+    'ui.router',
+    'ngAnimate',
+    'restangular',
+    'ui.bootstrap',
+    'app.controllers'
+])
+
+.config(['$stateProvider', '$urlRouterProvider', 'RestangularProvider',
+  function($stateProvider, $urlRouterProvider, RestangularProvider) {
+  RestangularProvider.setBaseUrl('https://sahara-health-api.herokuapp.com/');
+
+  RestangularProvider.addResponseInterceptor(function (data, operation, what, url, response, deferred) {
+      if (data.response && data.response.data) {
+          var returnedData = data.response.data;
+          return returnedData;
+      } else {
+          return data;
+      };
+  });
+  
+  $stateProvider
+    .state('home', {
+    url: '',
+    templateUrl: 'modules/home.html',
+    controller: 'appCtrl'
+  })
+    .state('results', {
+      url: '/bukola-saraki',
+      templateUrl: 'modules/search-result.html',
+      controller: 'resultCtrl'
+    })  
+
+  $urlRouterProvider.otherwise('/404')  
+}]);
